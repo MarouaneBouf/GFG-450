8fa92bac0b95c99e2b6ecb229bdf212879136456
MarouaneBouf <104838272+MarouaneBouf@users.noreply.github.com>
2022-07-15 19:49:44 +0100

    Update SortArray012.py

--- Array/SortArray012.py
+++ Array/SortArray012.py
@@ -1,11 +1,48 @@
-"""
-Input: 
-N = 5
-arr[]= {0 2 1 2 0}
-Output:
-0 0 1 2 2
-Explanation:
-0s 1s and 2s are segregated 
-into ascending order.
-"""
-arr = [0,2,0,1,2,0]
+
+#Input: 
+#N = 5
+#arr[]= {0 2 1 2 0}
+#Output:
+#0 0 1 2 2
+#Explanation:
+#0s 1s and 2s are segregated 
+#into ascending order.
+# method 01:
+
+def sort012(arr,n): # n = size of arr
+    nextZero = 0
+    nextTwo = n-1
+    i = 0
+    while i<=nextTwo:
+        if arr[i]==0:
+            arr[nextZero],arr[i] = arr[i],arr[nextZero]
+            nextZero+=1
+            i+=1
+        elif arr[i]==2:
+            arr[nextTwo],arr[i] = arr[i],arr[nextTwo]
+            nextTwo-=1
+        else:
+            i+=1
+    return arr
+
+#method 02: I did create a new array here :( 
+arr =[0,1,0,2,2,1]
+
+x = arr.count(0)
+y = arr.count(1)
+z = arr.count(2)
+L=[]
+
+while x>0:
+    L.append(0)
+    x-=1
+
+while y>0:
+    L.append(1)
+    y-=1
+
+while z>0:
+    L.append(2)
+    z-=1
+    
+print(L)
